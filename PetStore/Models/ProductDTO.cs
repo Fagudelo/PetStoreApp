057850c3effbb1ac5461e2207630326366aec789
Fagudelo <ferney.certifications@outlook.com>
2024-08-16 16:10:43 -0500

    Se crean las propiedades del modelo de datos 'ProductDTO'

--- PetStore/Models/ProductDTO.cs
+++ PetStore/Models/ProductDTO.cs
@@ -1,6 +1,19 @@
-﻿namespace PetStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetStore.Models
 {
     public class ProductDTO
     {
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+        [Required, MaxLength(100)]
+        public string Brand { get; set; } = string.Empty;
+        [Required, MaxLength(100)]
+        public string Category { get; set; } = string.Empty;
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public string Description { get; set; } = string.Empty;
+        public IFormFile? ImageFile { get; set; }
     }
 }
