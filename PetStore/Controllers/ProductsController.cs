eba1b5d1aca3e1bb2113338617ba74ebd1b7627c
Fagudelo <ferney.certifications@outlook.com>
2024-08-16 16:01:43 -0500

    Se crean el método 'Index'

--- PetStore/Controllers/ProductsController.cs
+++ PetStore/Controllers/ProductsController.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PetStore.Services;
 
 namespace PetStore.Controllers
 {
-    public class ProductsController : Controller
+    public class ProductsController(ApplicationDbContext applicationDbContext) : Controller
     {
+        private readonly ApplicationDbContext dbContext = applicationDbContext;
         public IActionResult Index()
+        {
+            var products = dbContext.Products.OrderByDescending(p => p.Id) .ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
