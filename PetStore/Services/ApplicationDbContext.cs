f8434c780e882e40ab4c28409ae6212569b973bb
Fagudelo <ferney.certifications@outlook.com>
2024-08-16 00:48:25 -0500

    Se agrega el DbSet para la creación de la tabla 'Products'

--- PetStore/Services/ApplicationDbContext.cs
+++ PetStore/Services/ApplicationDbContext.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PetStore.Models;
 
 namespace PetStore.Services
 {
-    public class ApplicationDbContext(DbContextOptions options) : DbContext
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext (options)
     {
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        //{
+            
+        //}
 
+        public DbSet<Product> Products { get; set; }
     }
 }
